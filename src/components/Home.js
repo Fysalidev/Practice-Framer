1fff8a6df14f37e8236d9e4d7ac8bc6da208f50a
ROBILLIARD Grégory <gregory.robilliard@icloud.com>
2023-04-11 10:26:24 +0200

    Loaders

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
+import Loader from "./Loader";
 
 const buttonVariants = {
   hover: {
@@ -25,7 +26,7 @@ const containerVariants = {
   exit: {
     x: "-100vw",
     transition: { ease: "easeInOut" },
-  }
+  },
 };
 
 const Home = () => {
@@ -43,6 +44,7 @@ const Home = () => {
           Create Your Pizza
         </motion.button>
       </Link>
+      <Loader />
     </motion.div>
   );
 };
