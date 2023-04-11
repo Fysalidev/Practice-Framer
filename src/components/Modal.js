da01091b62bcc1907cf8f35eddaf6b361bdafddc
ROBILLIARD Grégory <gregory.robilliard@icloud.com>
2023-04-11 09:51:15 +0200

    Modal animation part 2

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -5,7 +5,15 @@ import { motion, AnimatePresence } from "framer-motion";
 const backdrop = {
   visible: { opacity: 1 },
   hidden: { opacity: 0 },
-  exit: { opacity: 0 },
+};
+
+const modal = {
+  hidden: { y: "-100vh", opacity: 0 },
+  visible: {
+    y: "200px",
+    opacity: 1,
+    transition: { delay: 0.5 },
+  },
 };
 
 const Modal = ({ showModal, setShowModal }) => {
@@ -17,8 +25,15 @@ const Modal = ({ showModal, setShowModal }) => {
           variants={backdrop}
           initial="hidden"
           animate="visible"
-          exit="exit"
-        ></motion.div>
+          exit="hidden"
+        >
+          <motion.div className="modal" variants={modal}>
+            <p>Want to make another pizza?</p>
+            <Link to="/">
+              <button>Start again</button>
+            </Link>
+          </motion.div>
+        </motion.div>
       )}
     </AnimatePresence>
   );
